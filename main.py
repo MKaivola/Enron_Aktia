632b82593ed216690121eda9aa9874cb35d3ee24
MKaivola <52649339+MKaivola@users.noreply.github.com>
2020-01-22 16:31:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
 import os
+import email
+import csv
+
 
 rootDic = os.path.join(os.getcwd(),"enron_mail_20150507")
 
@@ -12,3 +15,27 @@ def folderWalk(keyword,folderFunc):
         for folder in os.listdir(emplFileP):
             if (folder.find(keyword) != -1):
                 folderFunc(os.path.join(emplFileP,folder))
+
+# Parses the emails in folder and writes the entries to file via entrywriter
+def extractContact(folder,entryvwriter):
+    # Dictionary to include all (sender,recipient) : count pairs
+    countDict = dict()
+    for file in os.listdir(folder):
+        # Read the email
+        f = open(os.path.join(folder,file),'r')
+        # Parse the email
+        content = email.parser.Parser().parsestr(f.read())
+        sender = content['From']
+        # Go through all the recipient headers
+        for header in ['To','cc','bcc']:
+            # Check whether the header is empty
+            if (content[header] != None):
+                # Extract the individual recipients
+                receivers = [name.strip() for name in content[header].split(', ')]
+                # Add the message to the dictionary
+                for receiver in receivers:
+                    key = (sender,receiver)
+                    countDict[key] = countDict.get(key,0) + 1
+    # Write the dictionary to the csv-file
+    for key,value in countDict.items():
+        entryWriter.writerow({'Sender': key[0],'Receiver': key[1],'Count': value})
