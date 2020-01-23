60a2b0c1feb88440ebe79a5246d3d5f51307bdd8
MKaivola <52649339+MKaivola@users.noreply.github.com>
2020-01-23 14:56:17 +0200

    Added comments

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import csv
 from datetime import datetime
 import numpy as np
 
+# Location of the employee directories
 rootDic = os.path.join(os.getcwd(),'enron_mail_20150507','maildir')
                 
-# Parses the emails in folder and writes the number of messages between
-# two email addresses to a csv-file as determined by entrywriter              
+# Parses the emails in folder and its subdirectories 
+# and writes the number of messages between
+# two email addresses to a csv-file as determined by entryWriter              
 def extractContact(folder,entryWriter):
-    # Dictionary to include all (sender,recipient) : count
+    # Dictionary to include all (sender,recipient) : count elements
     countDict = dict()
     def inner(subDic):
         for file in os.listdir(subDic):
@@ -37,16 +39,16 @@ def extractContact(folder,entryWriter):
     inner(folder)
     # Write the dictionary to the csv-file
     for key,value in countDict.items():
-        entryWriter.writerow({'sender': key[0],'receiver': key[1],'count': value})
+        entryWriter.writerow({'sender': key[0],'recipient': key[1],'count': value})
  
-# Calls extractContact for files in a folder 
+# Calls extractContact for files in directories 
 # whose name contains the substring "keyword"
 # Passes the write location along with the csvwriter
 def folderWalk(keyword,csvwriter):
     # Loop through the employees
     for empl in os.listdir(rootDic):
         emplFileP = os.path.join(rootDic,empl)
-        # Consider each folder separately
+        # Consider each directory separately
         for folder in os.listdir(emplFileP):
             if (folder.find(keyword) != -1):
                 extractContact(os.path.join(emplFileP,folder),csvwriter)
@@ -60,7 +62,7 @@ def dailyAverage(empl,inboxLoc,entryWriter):
     # How many given weekdays are observed
     dayCumu = np.zeros(7)
     # What dates have already been observed
-    observedDays = set()
+    observedDates = set()
     def inner(subDic):
         for file in os.listdir(subDic):
             currentPath = os.path.join(subDic,file)
@@ -78,10 +80,9 @@ def dailyAverage(empl,inboxLoc,entryWriter):
                 # Add email to cumulative 
                 emailCumu[dateEmail.weekday()] += 1
                 # If a new date is observed, add new observed weekday
-                if dateEmail.date() not in observedDays:
-                    observedDays.add(dateEmail.date())
+                if dateEmail.date() not in observedDates:
+                    observedDates.add(dateEmail.date())
                     dayCumu[dateEmail.weekday()] += 1
-    # Return zero if there are no emails for a given weekday
     inner(inboxLoc)
     # Return zero if there are no emails for a given weekday
     averageWeekday = np.divide(emailCumu,dayCumu,out = np.zeros_like(emailCumu),where = dayCumu != 0)
@@ -89,9 +90,11 @@ def dailyAverage(empl,inboxLoc,entryWriter):
         entryWriter.writerow({'employee': empl,'day_of_week': day, 'avg_count': averageWeekday[day]})
 
         
-# Call dailyAverage for all files in inbox folder
+# Calls dailyAverage for all files in inbox directories
+# and their subdirectories
 # Passes write location along with csvwriter        
 def inboxWalk(csvwriter):
+    # Loop through the employees
     for empl in os.listdir(rootDic):
         emplFileP = os.path.join(rootDic,empl)
         if 'inbox' in os.listdir(emplFileP):
@@ -100,7 +103,7 @@ def inboxWalk(csvwriter):
 # Task 1        
 with open('emails_sent_totals.csv','w') as totalFile:
     # Create a csv-writer for the file
-    entryWriter = csv.DictWriter(totalFile,fieldnames=['sender','receiver','count'])
+    entryWriter = csv.DictWriter(totalFile,fieldnames=['sender','recipient','count'])
     entryWriter.writeheader()
     folderWalk('sent',entryWriter)
 
